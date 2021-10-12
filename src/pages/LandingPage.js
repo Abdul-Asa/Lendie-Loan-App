b8374e02636a1ede1dbd7553d7aaddffdac3f002
OYELUSI ADEJOKE <funbiioye@gmail.com>
2021-10-12 23:26:14 +0100

    landing-page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,8 +1,11 @@
 // import {useState} from 'react';
-import { Heading, Center, Icon, Flex, Stack, Button } from '@chakra-ui/react';
+import { Heading, Icon, Flex, VStack, Button, } from '@chakra-ui/react';
 import { IoIosHourglass } from 'react-icons/io';
 import { useHistory } from 'react-router-dom';
 import { getUser } from '../utils/LocalStorage';
+import TopNav from '../components/entry-pages/TopNav';
+import HeroSection from '../components/entry-pages/HeroSection';
+import About from '../components/About';
 
 const LandingPage = () => {
   let history = useHistory();
@@ -11,22 +14,24 @@ const LandingPage = () => {
     history.push('/user/home');
   }
   return (
-    <Center h="90vh">
-      <Stack>
-        <Flex align="center">
-          <Icon as={IoIosHourglass} mr={2} boxSize={8} />
-          <Heading> Lendie App </Heading>
-        </Flex>
-        <Flex justify="space-around" pt={4}>
-          <Button size="lg" colorScheme="teal" as="a" href="/signup">
-            Signup
-          </Button>
-          <Button size="lg" colorScheme="teal" as="a" href="/login" ml={4}>
-            Login
-          </Button>
-        </Flex>
-      </Stack>
-    </Center>
+    <VStack bg='#fff' h='100%'>
+      <TopNav />
+      <HeroSection />
+      <About />
+      <Flex>
+        <Icon as={IoIosHourglass} mr={2} boxSize={8} />
+        <Heading> Lendie App </Heading>
+      </Flex>
+      <Flex justify="space-around" pt={4}>
+        <Button size="lg" colorScheme="teal" as="a" href="/signup">
+          Signup
+        </Button>
+        <Button size="lg" colorScheme="teal" as="a" href="/login" ml={4}>
+          Login
+        </Button>
+      </Flex>
+    </VStack>
+    
   );
 };
 
